feb5097c140f5d570f957556a179b6bf90ee2910
john <john.n.cameron@gmail.com>
2017-05-30 15:29:18 +0200

    add project page

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use((req, res, next) => {
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+ });
   next();
 });
 
@@ -25,7 +29,7 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+  return new Date().getFullYear()
 });
 
 hbs.registerHelper('screamIt', (text) => {
@@ -42,6 +46,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
